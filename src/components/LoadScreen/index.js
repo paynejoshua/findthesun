b46db8be675caa74b421f620ea6be1e402928d4e
Josh Payne <paynejoshua1@gmail.com>
2021-05-09 21:37:40 -0700

    added some basic button styling to buttons and logic to check if weather is sunny

---
+++ src/components/LoadScreen/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function LoadScreen(){
+
+    return(
+
+        <>
+        <h1>Hold On while we locate you</h1>
+        </>
+    )
+}
+
+export default LoadScreen
--- src/components/WeatherSearch/index.js
+++ src/components/WeatherSearch/index.js
@@ -3,32 +3,38 @@ import axios from "axios";
 
 
 function WeatherSearch(){
-    // const[results, setResults] = useState()
-    const[webApiKey, setWebApiKey] = useState("a4a8fbe7c144be93020e3a7f15b622db")
+    const[webApiKey] = useState("a4a8fbe7c144be93020e3a7f15b622db")
     const[lat, setLat] = useState()
     const[lon, setLon] = useState()
-
+    const[sunny, setSunny] = useState(false)
 
     useEffect(() => {
+
         navigator.geolocation.getCurrentPosition(function(position){
             setLat(position.coords.latitude)
             setLon(position.coords.longitude)
         })
 
+        getWeather()
+
+
         console.log(lat, lon)
         
-    })
+    }, [lat, lon])
 
     function getWeather(){
         axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${webApiKey}&units=imperial`)
         .then(function(res){
             console.log(res)
             
-            if(res.data.weather[0].icon !== "01d"){
-                console.log("It's not sunny")
-            } else {
-                
+            if(res.data.weather[0].icon == "01d" || "01n"){
+                setSunny(true)
+                console.log(sunny)
                 console.log("It's sunny")
+            } else {
+                setSunny(false)
+                console.log(sunny)
+                console.log("It's not sunny")
             }
             
         }).catch(function (error) {
@@ -44,8 +50,17 @@ function WeatherSearch(){
             <input placeholder="State:" onChange={((e) => setState(e.target.value))}></input>
             <button onClick={(() => switchSearch())}>Search</button> */}
 
-            <br />
-            <button onClick={(() => getWeather())}>Find that Sun</button>
+        
+
+        {
+            sunny 
+            ? <button> Where else is it sunny?</button>
+            : <button onClick={(() => getWeather())}>Find that Sun!</button>
+        }
+         
+        
+            
+            
             
         </>
     )
